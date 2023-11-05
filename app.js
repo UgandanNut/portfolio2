2e26bce8c1b9ab0c0230ca84bf59f33633a481e8
UgandanNut <122523233+UgandanNut@users.noreply.github.com>
2023-11-05 16:16:41 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,247 @@
+function sendmsg(){
+    alert("message is sent")
+    emailjs.send("service_16xlfep","template_81plbiv",{
+    from_name: document.getElementById("user_name").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value,
+    });}
+
+const hackgacor = document.getElementById("hackgacor")
+hackgacor.style.display = "none";
+
+    
+
+const gacorplay = document.getElementById("gacorplay");
+gacorplay.style.display = "none";
+
+    
+const phoneback = document.getElementById("phoneback")
+phoneback.style.display = "none";
+
+const amsg = document.getElementById("amsg")
+amsg.style.display = "none";
+
+const backk = document.getElementById("backk")
+backk.style.display = "none";
+
+const imag1 = document.getElementById("imag1")
+
+imag1.addEventListener("mouseover", function(){
+    imag1.style.opacity = "80%"
+})
+
+imag1.addEventListener("mouseout", function(){
+    imag1.style.opacity = "100%"
+})
+
+
+function img1(){
+    window.location.href = "https://www.skistudios.net/website2/"
+}
+
+
+var imag2 = document.getElementById("imag2")
+
+    imag2.addEventListener("mouseover", function(){
+        imag2.style.opacity = "70%"
+    })
+
+    imag2.addEventListener("mouseout", function(){
+        imag2.style.opacity = "100%"
+    })
+
+
+    function img2(){
+        window.location.href = "https://ugandannut.github.io/KALAMKUDUS-FORUM-IMLEK_/"
+    }
+
+    var imag3 = document.getElementById("imag3")
+
+    imag3.addEventListener("mouseover", function(){
+        imag3.style.opacity = "70%"
+    })
+
+    imag3.addEventListener("mouseout", function(){
+        imag3.style.opacity = "100%"
+    })
+
+
+    function img3(){
+        window.location.href = "https://ugandannut.github.io/bitcavee/index.html"
+    }
+
+
+    // about
+
+    const aboutt = document.getElementById("aboutt")
+
+    aboutt.addEventListener("mouseover", function(){
+        aboutt.style.opacity = "70%"
+    })
+
+    aboutt.addEventListener("mouseout", function(){
+        aboutt.style.opacity = "100%"
+    })
+    
+    //
+
+
+    function about() {
+        const imag1 = document.getElementById("imag1");
+        const imag2 = document.getElementById("imag2");
+        const imag3 = document.getElementById("imag3");
+        const client = document.getElementById("client");
+        const personal = document.getElementById("personal");
+        const backk = document.getElementById("backk");
+        const amsg = document.getElementById("amsg")
+        const contactform = document.getElementById("contactform")
+        const phoneimg = document.getElementById("phoneimg");
+        phoneimg.style.display = "none";
+        amsg.style.display = "block";
+        backk.style.display = "block";
+        contactform.style.display = "none";
+        imag1.style.display = "none";
+        imag2.style.display = "none";
+        imag3.style.display = "none";
+        client.style.display = "none";
+        personal.style.display = "none";
+        
+    }
+
+    function back() {
+        const imag1 = document.getElementById("imag1");
+        const imag2 = document.getElementById("imag2");
+        const imag3 = document.getElementById("imag3");
+        const client = document.getElementById("client");
+        const personal = document.getElementById("personal");
+        const backk = document.getElementById("backk")
+        const amsg = document.getElementById("amsg")
+        const contactform = document.getElementById("contactform")
+        const phoneback = document.getElementById("phoneback");
+        const phoneimg = document.getElementById("phoneimg")
+        phoneimg.style.display = "block";
+        phoneback.style.display = "none";
+        amsg.style.display = "none";
+        backk.style.display = "none";
+        contactform.style.display = "block";
+        imag1.style.display = "block";
+        imag2.style.display = "block";
+        imag3.style.display = "block";
+        client.style.display = "block";
+        personal.style.display = "block";
+       
+    }
+
+
+
+    function phoneopen() {
+        const imag1 = document.getElementById("imag1");
+        const imag2 = document.getElementById("imag2");
+        const imag3 = document.getElementById("imag3");
+        const client = document.getElementById("client");
+        const personal = document.getElementById("personal");
+        const backk = document.getElementById("backk");
+        const amsg = document.getElementById("amsg")
+        const contactform = document.getElementById("contactform")
+        const aboutt = document.getElementById("aboutt")
+        const phoneimg = document.getElementById("phoneimg");
+        const phoneback = document.getElementById("phoneback");
+        const gacorplay = document.getElementById("gacorplay");
+        gacorplay.style.display = "block";
+        phoneback.style.display = "block";
+        phoneimg.style.display = "none";
+        aboutt.style.display = "none";
+        amsg.style.display = "none";
+        backk.style.display = "none";
+        contactform.style.display = "display";
+        imag1.style.display = "none";
+        imag2.style.display = "none";
+        imag3.style.display = "none";
+        client.style.display = "none";
+        personal.style.display = "none";
+        
+    }
+
+    function phonebackk() {
+        const imag1 = document.getElementById("imag1");
+        const imag2 = document.getElementById("imag2");
+        const imag3 = document.getElementById("imag3");
+        const client = document.getElementById("client");
+        const personal = document.getElementById("personal");
+        const backk = document.getElementById("backk")
+        const amsg = document.getElementById("amsg")
+        const contactform = document.getElementById("contactform");
+        const phoneback = document.getElementById("phoneback");
+        const about = document.getElementById("aboutt");
+        const phoneimg = document.getElementById("phoneimg")
+        const gacorplay = document.getElementById("gacorplay");
+        gacorplay.style.display = "none";
+        phoneimg.style.display = "block";
+        phoneback.style.display = "none";
+        aboutt.style.display = "block"
+        amsg.style.display = "none";
+        backk.style.display = "none";
+        contactform.style.display = "block";
+        imag1.style.display = "block";
+        imag2.style.display = "block";
+        imag3.style.display = "block";
+        client.style.display = "block";
+        personal.style.display = "block";
+       
+    }
+    
+
+
+    //gacor
+    function hack() {
+        const gas = document.getElementById("gas").innerHTML = "7777"
+    }
+
+
+    function claim() {
+    const gas = document.getElementById("gas").innerHTML;
+    if(gas == "7777"){
+        alert("you win 1000000$");
+        document.getElementById("gas").innerHTML = "0";
+        } else{
+            alert("didn't win anything")
+            
+        }
+    }
+
+    uangdep = 0;
+    document.getElementById("totaldep").innerHTML = "total deposit =" + uangdep + "$";
+    
+    function deposit(){
+    uangdep = 100;
+    document.getElementById("totaldep").innerHTML = "total deposit =" + uangdep + "$";
+    }
+
+    
+
+
+    function gacor(min, max){ 
+        
+        if(uangdep < 100){
+            alert("deposit some money first!")
+        }
+        if(uangdep === 100){
+        document.getElementById("gas").innerHTML =
+        Math.floor(Math.random() * (max - min + 1)) + min;
+        uangdep = 0;
+        document.getElementById("totaldep").innerHTML = "total deposit =" + uangdep + "$";
+        } 
+       
+    }
+
+
+        function confirmhackpass() {
+            const hackgacor = document.getElementById("hackgacor");
+            const hackinput = document.getElementById("hackinput").value;
+            if(hackinput === "3718"){
+             hackgacor.style.display = "block";
+             alert("HACK unlocked!");
+            } else {
+                alert("wrong password")
+            }
+        }
